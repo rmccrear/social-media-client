09571dc6ce8447690d346fb376d8121080b32b9c
Robert McCreary <113715288+rmccrear@users.noreply.github.com>
2024-04-07 22:31:22 +0000

    wrap App redux Provider and react Router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import "./App.css"
 import Auth from "./pages/Auth/Auth";
 import Home from "./pages/home/Home";
 import Profile from "./pages/Profile/Profile";
+
+import { Provider } from 'react-redux'
+import store from './store/ReduxStore'
+
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-        <div className="blur" style={{top: '-18%', right: '0'}}></div>
-        <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
-        
-        <Auth/>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+            <div className="blur" style={{top: '-18%', right: '0'}}></div>
+            <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
+            
+            <Auth/>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
